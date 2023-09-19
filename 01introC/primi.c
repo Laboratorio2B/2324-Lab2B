4ea857021b1329d27702a6b8d9c4b1c6cf16646f
giovanni <giovanni.manzini@gmail.com>
2023-09-19 11:36:50 +0200

    lezione 1

---
+++ 01introC/primi.c
@@ -0,0 +1,98 @@
+#define _GNU_SOURCE   // avverte che usiamo le estensioni GNU 
+#include <stdio.h>    // permette di usare scanf printf etc ...
+#include <stdlib.h>   // conversioni stringa/numero exit() etc ...
+#include <stdbool.h>  // gestisce tipo bool (per variabili booleane)
+#include <assert.h>   // permette di usare la funzione assert
+#include <string.h>   // funzioni di confronto/copia/etc di stringhe
+
+// le istruzioni qui sopra le spieghiamo più avanti 
+
+// da compilare con:
+//  gcc -std=c11 -Wall -O -g -o sommad  sommad.c
+
+
+// Scopo del programma:
+//  legge un intero N da tastiera
+//  crea un array *dinamico* con i primi <=N
+
+// prototipo della funzione primo()
+// bool primo(int k);
+// non necessario in quanto la definizione è prima del main
+
+
+
+// dato k restituisco True se è primo, false altrimenti
+// estremamente inefficiente!!!
+bool primo(int k)
+{
+  for(int i=2;i<k;i++)
+    if(k%i==0) return false; // ho scoperto che il numero non è primo
+  return true;
+}
+
+
+// stampa un messaggio d'errore e termina il programma
+void termina(char *messaggio)
+{
+  puts(messaggio);
+  exit(1);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+  int n; // definisco variabile intera di nome n
+
+  // spiego cosa voglio leggere e lo leggo con scanf  
+  printf("Inserisci il valore N: ");
+  int e = scanf("%d",&n); // il motivo della & lo vedremo più avanti 
+  // controlli sulla lettura
+  if(e!=1) termina("Valore non trovato");
+  if(n<2)  termina("Non ci sono numeri primi");
+  
+  
+  // crea un array dinamico inizialmente di 10 elementi
+  int *a;  // dichiaro che a sarà usata come array
+  int size = 10;   // dimensione attuale dell'array
+  int messi = 0;   // numero di elementi attualmente dentro l'array 
+  a  = malloc(size*sizeof(int));
+  if(a==NULL) 
+    termina("Malloc fallita");
+
+
+  // riempio array
+  for(int i=2; i<=n; i++) {
+    if(primo(i)) {
+      // se l'intero i è primo lo inserisco
+      // nella tabella dei primi
+      // ma prima verifico che ci sia spazio
+      if(messi==size) {
+        size = 2*size; // se la tabella è piena raddoppio la dimensione
+        a = realloc(a,size*sizeof(int));
+        if(a==NULL)
+          termina("Realloc fallita");
+      }
+      // inserisco il primo i dentro a[]
+      a[messi] = i;
+      messi += 1;
+    }
+  }
+  // tabella completata
+  // riduco array alla dimensione minima
+  a = realloc(a,messi*sizeof(int));
+  if(a==NULL) termina("Realloc fallita");
+    
+  // stampa contenuto array, usando 8 caratteri per itero 
+  for(int i=0;i<=messi;i++) {     
+    printf("%8d",a[i]);                    
+  }
+  printf("\n");
+  // dealloco la memoria della tabella
+  free(a);
+  return 0;
+}
+
+
+
+
---
+++ Registro.md
@@ -0,0 +1,50 @@
+
+##  Introduzione ad array, puntatori e file (introC)
+
+
+**Lezione 1** (19/9/23)
+
+* Introduzione al linguaggio C. Esempio di lettura e scrittura di interi dal terminale. Compilazione. Esecuzione di programmi 
+
+-----------------------
+
+**Lezione 2** (22/9/23)
+
+* Allocazione dinamica di array in C. Funzioni: malloc/free/realloc. 
+* introduzione al *makefile*; compilazione con il comando *make*
+* Utilizzo di *valgrind* per verificare il corretto utilizzo degli array
+
+-------
+
+**Lezione 3** (26/9/23)
+
+* Introduzione ai puntatori
+* Significato e uso di `*` e `&`
+* Esempio passaggio parametri per riferimento
+
+-------
+
+**Lezione 4** (29/9/23)
+
+* Ancora sui puntatori
+* Rappresentazione di stringhe in C
+* Uso dei parametri argc/argv
+
+-----
+
+**Lezione 5** (3/10/23)
+
+* Ancora su argc/argv
+* Copia di un puntatore vs copia di un array: la funzione strdup()
+* Scrittura in un file di testo: fopen(), fprintf(), fclose()
+
+-----
+
+**Lezione 6** (4/10/23)
+
+* Uso di asserzioni
+* lettura di un array di interi da un file
+* stdout/stderr/stdin e loro ridirezione
+* funzione perror() e variabile globale `errno`
+* esercizio su array
+
